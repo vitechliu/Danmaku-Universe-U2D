62c89a56e34dc8cea1fd319328d71a1f3a43e824
liuzirui1122 <liuzirui1122@163.com>
2016-11-03 11:20:19 +0800

    1

--- Assets/class/Particles.cs
+++ Assets/class/Particles.cs
@@ -7,29 +7,41 @@ public class Particles : MonoBehaviour {
 	private float size;
 	private float blur;
 	private Color color;
-	[SerializeField]public static float minSize = 1;
-	[SerializeField]public static float maxSize =3;
+	public float minSize = 0.01f;
+	public float maxSize = 0.05f;
 	private SpriteRenderer sr;
 	// Use this for initialization
 	void Start () {
 		sr = GetComponent<SpriteRenderer> ();
 		size = Random.Range (minSize, maxSize);
 		if (Random.Range(1,11)<2) {
+			
 			color = DUlib.randColor ();
 			sr.color = color;
 		}
 		DUlib.Hello ();
 		float x = Random.Range(0f,Screen.width);
 		float y = Random.Range(0f,Screen.height);
+		float z = Random.Range(12f,50f);
+		Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(x,y,z));
 
-		Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector2(x,y));
 
-		pos.z = Random.Range(1f,10f);
 		transform.position = pos;
+		transform.localScale = new Vector3 (size, size, 1f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+		if (pos.x < 0)
+			pos.x = Screen.width;
+		else if (pos.x > Screen.width)
+			pos.x = 0;
+		if (pos.y < 0)
+			pos.y = Screen.height;
+		else if (pos.y > Screen.height)
+			pos.y = 0;
+		transform.position = Camera.main.ScreenToWorldPoint (pos);
+		//Vector3 pos = Camera.main.WorldToScreenPoint(
 	}
 }
---
+++ Assets/gameController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameController : MonoBehaviour {
+	public GameObject particles;
+	// Use this for initialization
+	void Start () {
+		for (int i=0;i<200;i++) Instantiate (particles);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
